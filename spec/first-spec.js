e561eb5881fe5da4ed866c549f4f5cfd7b1f816f
aboynejames <james@aboynejames.co.uk>
2012-10-03 15:34:32 +0100

    tidy up test buster unit test

--- spec/first-spec.js
+++ spec/first-spec.js
@@ -1,17 +1,5 @@
-//var buster = require("buster");
-/*
-var myLib = require("../src/index.js");
 
-buster.testCase("A test case", {
-    "test it": function () {
-        assert(true);
-    }
-})
-*/
-
-//if (typeof module == "object" && typeof require == "function") {
     var buster = require("buster");
-//}
 
 buster.spec.expose(); // Make spec functions global
 
@@ -29,20 +17,3 @@ var spec = describe("basic node test", function () {
   	
 		
 	});  // closes spec
-
-
-
-/*
-buster.testCase("Multi-environment", {
-    "runs in all environments": function () {
-        assert(true);
-    },
-
-    "sub context": {
-        requiresSupportFor: { "DOM": typeof document != "undefined" },
-
-        "only runs in browser-like environments": function () {
-            // ...
-        }
-    }
-*/
